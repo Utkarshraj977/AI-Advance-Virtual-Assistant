99e0c8979206d6bdfdd47504a5350ce34a31db69
Utkarsh Raj <utkarshbrb6@gmail.com>
2026-01-29 00:24:47 +0530

    Update API key usage and model version in gemini.js

--- src/gemini.js
+++ src/gemini.js
@@ -1,14 +1,14 @@
-
+const apiKey = import.meta.env.API_KEY;
 import {
     GoogleGenerativeAI,
     HarmCategory,
     HarmBlockThreshold,
   } from "@google/generative-ai";
   
-  const genAI = new GoogleGenerativeAI(apikey);
+  const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro-latest",
+    model: "gemini-2.0-flash",
   });
   
   const generationConfig = {
